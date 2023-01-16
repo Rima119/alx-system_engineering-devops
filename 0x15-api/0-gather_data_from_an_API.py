d717cedac8e32f41b567426ee632a29abb3d02b7
Rim El Filali <rimo1902@gmail.com>
2023-01-16 10:28:40 -0800

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == '__main__':
     if len(argv) > 1:
-        userId = argv[1]
+        userId = int(argv[1])
         API_URL = 'https://jsonplaceholder.typicode.com'
         user_res = requests.get('{}/users/{}'.format(API_URL, userId)).json()
         todo_res = requests.get('{}/todos'.format(API_URL)).json()
@@ -18,8 +18,7 @@ if __name__ == '__main__':
         completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
         NUMBER_OF_DONE_TASKS = len(completed_tasks)
         TOTAL_NUMBER_OF_TASKS = len(tasks)
-        print("Employee {} is done with tasks({}/{}):"
-              .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
-              TOTAL_NUMBER_OF_TASKS))
-        for todo in completed_tasks:
-            print('\t {}'.format(todo.get('title')))
+        print("Employee {} is done with tasks({}/{}):".format(
+              EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for todo in completed_tasks:
+        print('\t {}'.format(todo.get('title')))
