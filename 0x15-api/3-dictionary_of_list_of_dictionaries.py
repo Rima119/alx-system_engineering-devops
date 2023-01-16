c19a71b61a1f40710f67a63bd0988c8b11cab3f3
Rim El Filali <rimo1902@gmail.com>
2023-01-16 11:07:44 -0800

    add

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script to export data in the JSON format """
+import json
+import requests
+
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users/{}"
+    user_name = requests.get(url, verify=False).json()
+    list_tasks = {}
+    for user in user_name:
+        todos = requests.get(url + '/{}/todos'.format(user['id'])).json()
+        list_tasks[user['id']] = [{'task': task.get('title'),
+                                   'completed': task.get('completed'),
+                                   'username': user['username']}
+                                  for task in todos]
+    with open("todo_all_employees.json", 'w', newline='') as jsonfile:
+        json.dump(list_tasks, jsonfile)
