0607878a4bc82e96a7b3b912efe7527a9d173cd8
Rim El Filali <rimo1902@gmail.com>
2023-01-31 20:30:00 +0000

    add

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API"""
+import requests
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "My-User-Agent"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
