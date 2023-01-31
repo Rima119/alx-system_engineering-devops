bf027204959fb940d73b74cc9852a5aae11b58c5
Rim El Filali <rimo1902@gmail.com>
2023-01-31 20:39:44 +0000

    add

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,8 @@ import requests
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts listed for a given subreddit
+    Prints the titles of the first 10 hot posts listed
+    for a given subreddit
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
@@ -16,7 +17,6 @@ def top_ten(subreddit):
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    
     if response.status_code == 404:
         print("None")
         return
