d721bd22b55c5e89a1d33899db0e9c8949fb8fd3
Rim El Filali <rimo1902@gmail.com>
2023-01-31 20:40:58 +0000

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API"""
+'''Function that queries the Reddit API'''
 import requests
 
 
 def top_ten(subreddit):
-    """
+    '''
     Prints the titles of the first 10 hot posts listed
     for a given subreddit
-    """
+    '''
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "My-User-Agent"
